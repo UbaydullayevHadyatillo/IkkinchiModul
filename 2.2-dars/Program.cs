4b4df0d964c1eef72e1d2b4ba4b9a74ae89aeaf3
UbaydullayevHadyatillo <ubaydullayevhp@gmail.com>
2026-01-23 00:29:53 +0500

    2-dars

---
+++ 2.2-dars/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace _2._2_dars;
+
+internal class Program
+{
+    static List<Book> Books = new List<Book>();
+    static void Main(string[] args)
+    {
+        DataSeed();
+        var book = GetMaxPricedBook();
+        Console.WriteLine(book.Price);
+    }
+
+    static Book GetMaxPricedBook()
+    {
+        Book book = Books[0];
+
+        foreach(var b in Books)
+        {
+            if(book.Price<b.Price)
+            {
+                book = b;
+            }
+        }
+        return book;
+    }
+
+    static void DataSeed()
+    {
+        Books.Add(new Book()
+        {
+            BookId = Guid.NewGuid(),
+            Title = "Jumavoy",
+            Author = "Robinzon Kuruzo",
+            Price = 10,
+            Description = "sarguzasht",
+            Created = DateTime.Now,
+            Genre = "qzqarli",
+            PgeCount = 200,
+        });
+
+        Books.Add(new Book()
+        {
+            BookId = Guid.NewGuid(),
+            Title = "sariq devni minib",
+            Author = "Xudoyberdi Tohtaboyev",
+            Price = 9,
+            Description = "Fantastika",
+            Created = DateTime.Now,
+            Genre = "kamedya",
+            PgeCount = 180,
+        });
+
+        Books.Add(new Book()
+        {
+            BookId = Guid.NewGuid(),
+            Title = "Harry poter",
+            Author = "opa",
+            Price = 19,
+            Description = "Fantastika",
+            Created = DateTime.Now,
+            Genre = "Fantastika",
+            PgeCount = 380,
+        });
+
+    }
+}
