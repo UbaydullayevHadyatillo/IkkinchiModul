abce7f2dd88645476719a2a73b92e1d475ac8961
UbaydullayevHadyatillo <ubaydullayevhp@gmail.com>
2026-01-23 00:18:55 +0500

    2.1-dars

---
+++ 2.1-dars/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace _2._1_dars;
+
+internal class Product
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public DateTime DateRelease { get; set; }
+    public  DateTime BestBefore { get; set; }
+    public DateTime ExpirationDate { get; set; }
+}
---
+++ 2.1-dars/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _2._1_dars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Name = "Banana";
+            product.Price = 18000;
+            product.DateRelease = DateTime.Now;
+            product.BestBefore = DateTime.Now;
+        }
+    }
+}
